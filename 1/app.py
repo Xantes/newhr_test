0012ca9cb1bd400be3e1e12ef05eb92cca5e9d9d
Xantes <a1793@yandex.ru>
2024-02-12 09:45:00 +0900

    Final

--- 1/app.py
+++ 1/app.py
@@ -1,11 +1,22 @@
-from requests import get
+from requests import get, Response, RequestException
+
+
+def get_response(url=None):
+    try:
+        response: Response = get(url)
+    except RequestException as error:
+        raise SystemExit(error)
+
+    return response.json()
+
 
 def app():
-    req_url = 'https://api.ipify.org?format=json'
-    geo_url = 'http://ip-api.com/json/'
 
-    ip  = get(req_url).json()
-    geo = get(geo_url).json()
+    req_url: str = 'https://api.ipify.org?format=json'
+    geo_url: str = 'http://ip-api.com/json/'
+
+    ip: dict[str, str] = get_response(req_url)
+    geo: dict[str, str] = get_response(geo_url)
 
     print('IP: ', ip.get('ip'), '\nREGION: ', geo.get('region'))
 
